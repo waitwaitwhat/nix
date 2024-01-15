3f324ec6956c29bceda1a9a44134c084289302d7
waitwaitwhat <156558595+waitwaitwhat@users.noreply.github.com>
2024-01-15 14:37:04 +1100

    Update w8.nix

--- modules/users/w8.nix
+++ modules/users/w8.nix
@@ -1,1 +1,12 @@
+{ config, pkgs, ... }:
 
+{ 
+  users.users = { 
+    w8 = {
+      isNormalUser = true;
+      home = "/home/w8";
+      extraGroups = [ "wheel" "networkmanager" "homemanager" "nixbld" "video" "audio" ];
+      #openssh.authorizedKeys.keys = [ "ssh-dss AAAAB3Nza... alice@foobar" ];
+    };
+  };
+}
