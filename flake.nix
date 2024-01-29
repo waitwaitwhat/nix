ab22236137ad0d29d8aaf9d3e0bd9223a4ff2bcd
waitwaitwhat <156558595+waitwaitwhat@users.noreply.github.com>
2024-01-29 16:22:14 +1100

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -1,1 +1,61 @@
-TBD
+{
+  description = "w8's tux system flake";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+      # 23.11-url = "github:nixos/nixpkgs/nixos-23.11"
+      # random-url = "github:nixos/nixpkgs/HASH"
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+ 
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs = { 
+    self, 
+    nixpkgs,
+    ...
+    }@inputs: 
+  let 
+  inherit (self) outputs;
+    system = [
+    # "aarch64-linux"
+    "x86_64-linux"
+    ];
+    # This is a function that generates an attribute by calling a function you
+    # pass to it, with each system as an argument
+    forAllSystems = nixpkgs.lib.genAttrs system;
+
+    pkgs = import nixpkgs {
+      inherit inputs system;
+
+      config = {
+        allowUnfree = true;
+      };
+    };
+
+  in
+  {
+  # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      # FIXME replace with your hostname
+      tux = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs system;};
+        modules = [
+          # > Our main nixos configuration file < 
+          ./nixos/hosts/default/configuration.nix
+        ];
+      };
+    }; 
+  };
+}
