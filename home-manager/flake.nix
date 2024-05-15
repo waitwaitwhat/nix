a7d75dde2129edacc51467ca65248e69453563f1
waitwaitwhat <czekacwhat@protonmail.com>
2024-05-15 23:13:55 +1000

    wtf

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -17,7 +17,7 @@
       owner = "lexical-lsp";
       repo = "lexical";
     };
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
   };
 
   outputs = { nixpkgs, home-manager, hyprland, ... }@inputs:
