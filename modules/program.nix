927290ca80bb97984fd3b661600b0179ea6c204f
waitwaitwhat <czekacwhat@protonmail.com>
2024-05-20 15:36:35 +1000

    fix missing semicolons

--- modules/program.nix
+++ modules/program.nix
@@ -16,8 +16,8 @@
     programs.nh = {
       enable = true;
       clean.enable = true;
-      clean.extraArgs = "--keep-since 4d --keep 3"
-      flake "/home/w8/flake"
+      clean.extraArgs = "--keep-since 4d --keep 3";
+      flake "/home/w8/flake";
     };
     steam = {
       enable = true;
