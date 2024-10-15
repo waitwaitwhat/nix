b32d1fae3b56ad492f5566055129fdca9f6155cc
wait <156558595+waitwaitwhat@users.noreply.github.com>
2024-10-15 15:18:40 +1100

    Update configuration.nix

--- hosts/macbook/configuration.nix
+++ hosts/macbook/configuration.nix
@@ -1,1 +1,39 @@
+{ config, pkgs, inputs, lib, self, ... }:
 
+{
+  imports =
+    [
+      # ../../modules/aagl.nix
+      ../../modules/bootloader.nix
+      ../../modules/cosmic.nix
+      ../../modules/font.nix
+      # ../../modules/greetd.nix
+      ../../modules/hardware.nix
+      ./hardware-configuration.nix
+      ./disk-config.nix
+      ../../modules/japanese.nix
+      ../../modules/network.nix
+      ../../modules/packages.nix
+      ../../modules/pipewire.nix
+      ../../modules/program.nix
+      # ../../modules/virtualisation.nix
+      ../../modules/security.nix
+      ../../modules/services.nix
+      ../../modules/system.nix
+      ../../modules/user.nix
+  ];
+  
+  networking.hostName = "macbook";
+
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi";
+    };
+    grub = {
+      enable = true;
+      efiSupport = true;
+      device = "nodev"
+    };
+  };
+}
